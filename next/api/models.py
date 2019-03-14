ff6165413523f3e869bc2beee794ec5537d3a4fd
Abhijith Neil Abraham <abhijithneilabrahampk@gmail.com>
2019-03-14 20:05:10 +0530

    up

--- next/api/models.py
+++ next/api/models.py
@@ -1,3 +1,9 @@
 from django.db import models
-
+class Note(models.Model):
+    title = models.CharField(max_length=200)
+    body = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
 # Create your models here.
+
+def __str__(self):
+        return '%s %s' % (self.title, self.body)
