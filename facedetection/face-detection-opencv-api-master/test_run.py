714db056261172764333156eb101b50fb061e7a7
Abhijith Neil Abraham <abhijithneilabrahampk@gmail.com>
2019-07-28 13:20:42 +0530

    Update facedetection/face-detection-opencv-api-master/test_run.py

--- facedetection/face-detection-opencv-api-master/test_run.py
+++ facedetection/face-detection-opencv-api-master/test_run.py
@@ -1,12 +1,9 @@
 import cv2, requests
 url = "http://localhost:8000/face_detection/detect/"
 ##### image 1 #####
-image_to_read = cv2.imread("C:/Punisher/1 UTD/Spring 18/facedetection/image5.jpg")
+image_to_read = cv2.imread("image5.jpg")
 tracker = {"url": "https://image.ibb.co/cPrdgS/image5.jpg"}
 req = requests.post(url, data=tracker).json()
-print
-"image3.png: {}".format(req)
-
 
 for (w,x,y,z) in req["faces"]:
     cv2.rectangle(image_to_read,(w,x), (y,z), (0, 255, 0), 2)
